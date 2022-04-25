923c92a191fe8874318ec18aebca56ef147a9c10
Gianluca Spada <gianluca.spada@spindox.it>
2022-04-25 14:36:23 +0200

    Update random product code

--- src/functions/randomProducts/utils.ts
+++ src/functions/randomProducts/utils.ts
@@ -1,14 +1,14 @@
-export const getRandomElements = <T>(array: T[], count: number) => {
-  const result = []
-  const seenIndexes = new Set()
-  const target = array.length < count ? array.length : count
-
-  while (result.length < target) {
-    const randomIndex = Math.floor(Math.random() * array.length)
-    if (seenIndexes.has(randomIndex)) continue
-    seenIndexes.add(randomIndex)
-    result.push(array[randomIndex])
+// Durstenfeld's shuffle algorithm
+const shuffleInPlace = (array: any[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
   }
+}
 
-  return result
+export const getRandomElements = <T>(array: T[], count: number) => {
+  shuffleInPlace(array)
+  return array.slice(0, count)
 }
