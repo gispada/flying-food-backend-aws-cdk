b9e3d6f043e8ad94e55ed427876d479150d58368
Gianluca Spada <gianluca.spada@spindox.it>
2023-03-26 18:37:37 +0200

    Update seed script and data

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -11,6 +11,9 @@ import { APP_NAME, DATA_PATH } from '../lib/constants'
  * Table names in DynamoDB need to match this format: {APP_NAME}_{filename_without_extension}
  */
 
+// Max number of items per batch: https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html
+const BATCH_SIZE = 25
+
 const isNotDdbFile = (file: string) => !(/.*\.ddb\.json$/).test(file)
 
 const getDataPath = (path = '') => join(__dirname, `../${DATA_PATH}/${path}`)
@@ -18,35 +21,40 @@ const getDataPath = (path = '') => join(__dirname, `../${DATA_PATH}/${path}`)
 const dataFiles = readdirSync(getDataPath()).filter(isNotDdbFile)
 
 for (const filename of dataFiles) {
-  const content: Record<string, PutRequest>[] = []
   const [resourceName] = filename.split('.')
+  const batches: Record<string, PutRequest>[][] = []
 
   const fileContent = readFileSync(getDataPath(filename), { encoding: 'utf-8' })
+  const itemsToInsert = JSON.parse(fileContent) as Record<string, any>[]
 
-  for (const item of JSON.parse(fileContent)) {
-    content.push({ PutRequest: { Item: DynamoDB.Converter.marshall(item) } })
-  }
+  itemsToInsert.forEach((item, i) => {
+    const batchIdx = Math.floor(i / BATCH_SIZE)
+    batches[batchIdx] ??= []
+    batches[batchIdx].push({ PutRequest: { Item: DynamoDB.Converter.marshall(item) } })
+  })
 
-  writeFileSync(
-    getDataPath(`${resourceName}.ddb.json`),
-    JSON.stringify({ [`${APP_NAME}_${resourceName}`]: content })
-  )
+  batches.forEach((content, i) => {
+    writeFileSync(
+      getDataPath(`${resourceName}-${i + 1}.ddb.json`),
+      JSON.stringify({ [`${APP_NAME}_${resourceName}`]: content })
+    )
 
-  exec(
-    `aws dynamodb batch-write-item --request-items file://${getDataPath(`${resourceName}.ddb.json`)}`,
-    (error, stdout, stderr) => {
-      if (error || stderr) {
-        console.log(`Error: ${error?.message || stderr}`)
-        return
+    exec(
+      `aws dynamodb batch-write-item --request-items file://${getDataPath(`${resourceName}-${i + 1}.ddb.json`)}`,
+      (error, stdout, stderr) => {
+        if (error || stderr) {
+          console.log(`Error: ${error?.message || stderr}`)
+          return
+        }
+
+        const { UnprocessedItems } = JSON.parse(stdout)
+
+        if (Object.keys(UnprocessedItems).length === 0) {
+          console.log(`> ${content.length} '${resourceName}' successfully imported`)
+        } else {
+          console.log(stdout)
+        }
       }
-
-      const { UnprocessedItems } = JSON.parse(stdout)
-
-      if (Object.keys(UnprocessedItems).length === 0) {
-        console.log(`> '${resourceName}' successfully imported`)
-      } else {
-        console.log(stdout)
-      }
-    }
-  )
+    )
+  })
 }
